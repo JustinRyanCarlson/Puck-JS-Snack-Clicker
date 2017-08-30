85ce8626a786ab45d8721672a285dc62fe7bf7ec
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-08-30 11:56:26 -0500

    removes some bot calls

--- server.js
+++ server.js
@@ -21,18 +21,11 @@ app.get("/", function(req, res) {
 
 app.get("/snack", function(req, res) {
         // define channel, where bot exist. You can adjust it there https://my.slack.com/services  
-        bot.postMessageToChannel('snackbottest', 'meow!', params);
+        bot.postMessageToChannel('snackbottest', 'Snacks are in the house!', params);
         
         // define existing username instead of 'user_name' 
-        bot.postMessageToUser('justin.carlson', '/giphy ', params); 
-        
-        // If you add a 'slackbot' property,  
-        // you will post to another user's slackbot channel instead of a direct message 
-        bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
-        
-        // define private group instead of 'private_group', where bot exist 
-        bot.postMessageToGroup('private_group', 'meow!', params); 
-        
+        bot.postMessageToUser('justin.carlson', 'Snack notifications sent successfully', params); 
+
         res.status(200).send("success");
 })
 
