ecd555a4b8acd030387a786b9ca389b0c712c40a
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-08-30 10:44:41 -0500

    fixes slackbot error issue

--- server.js
+++ server.js
@@ -1,33 +1,18 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 var SlackBot = require('slackbots');
 
 // Create bot 
 var bot = new SlackBot({
-    token: 'xoxb-233947548631-VRyZ0Lwe6LMZU8Q1BtyuiMVy', 
+    token: 'xoxb-233947548631-mKxOdJpjKATyoJpzItYSLjfJ', 
     name: "Snacky Snackerton"
 });
 
- // What the bot does when connection to slack is established 
-bot.on('start', function() {
-    // more information about additional params https://api.slack.com/methods/chat.postMessage 
-    var params = {
-        icon_emoji: ':popcorn:'
-    };
-    
-    // define channel, where bot exist. You can adjust it there https://my.slack.com/services  
-    bot.postMessageToChannel('snackbottest', 'meow!', params);
-    
-    // define existing username instead of 'user_name' 
-    bot.postMessageToUser('justin.carlson', '/giphy ', params); 
-    
-    // If you add a 'slackbot' property,  
-    // you will post to another user's slackbot channel instead of a direct message 
-    bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
-    
-    // define private group instead of 'private_group', where bot exist 
-    bot.postMessageToGroup('private_group', 'meow!', params); 
-});
+// more information about additional params https://api.slack.com/methods/chat.postMessage 
+var params = {
+    icon_emoji: ':popcorn:'
+};
 
 // Set up a URL route
 app.get("/", function(req, res) {
@@ -47,6 +32,8 @@ app.get("/snack", function(req, res) {
         
         // define private group instead of 'private_group', where bot exist 
         bot.postMessageToGroup('private_group', 'meow!', params); 
+        
+        res.status(200).send("success");
 })
 
 // bind the app to listen for connections on a specified port
