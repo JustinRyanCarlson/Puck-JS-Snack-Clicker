09a677ea5cd6c8c84bfc2292179a3350b6a88600
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-08-31 14:52:11 -0500

    token

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'static_files')));
 
 // Create bot 
 var bot = new SlackBot({
-    token: 'xoxb-233947548631-kHjbKNcyCq1r5fBECtyd7cPq', 
+    token: 'xoxb-233947548631-25SBJ9z5JhXRLqti4bXkEqsw', 
     name: "Mr. Snacks"
 });
 
