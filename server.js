f6189ff9149678b76df9a1bc5960e799da48338c
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-09-05 21:51:34 -0500

    uses a new package to interact with the slack api d ue to token timeouts

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-var SlackBot = require('slackbots');
+var slackAPI = require('slackbotapi');
 
 app.use(express.static(path.join(__dirname, 'static_files')));
 
-// Create bot 
-var bot = new SlackBot({
-    token: 'xoxb-233947548631-olY8nkFH76g8fRFWj1lITYD1', 
-    name: "Mr. Snacks"
+// Starting bot connection
+var slack = new slackAPI({
+    'token': 'xoxb-233947548631-Ku5se06wH4V41H87E1UQn8VT',
+    'logging': true,
+    'autoReconnect': true
 });
-
-// params for bot
-var params = {
-    icon_emoji: ':popcorn:'
-};
 
 var slackMessage = ":green_apple::pear::banana::peach::apple::banana::banana::banana::banana::tada:" + 
     ":the_horns::sunglasses::the_horns::tada::banana::banana::banana::banana::apple::peach::banana:" + 
@@ -30,25 +26,25 @@ app.get("/", function(req, res) {
 
 app.get("/snack9", function(req, res) {
     // post to channel for workers on floor 9
-    bot.postMessageToChannel('snacks_9th', slackMessage, params);
+    slack.sendMsg('snacks_9th', slackMessage);
 
     res.status(200).send("success");
 })
 
 app.get("/snack11", function(req, res) {
     // post to channel for workers on floor 11 
-    bot.postMessageToChannel('snacks_11th', slackMessage, params);
-        
-    // CHANGEME to brooke
-    bot.postMessageToUser('justin.carlson', 'Snack notifications sent successfully', params); 
+    slack.sendMsg('snacks_11th', slackMessage);
 
+    // post msg to Brooke 
+    slack.sendPM('brooke', 'Snack notification sent sucessfully');
+        
     res.status(200).send("success");
 })
 
 app.get("/battery", function(req, res) {        
-    // define existing username instead of 'user_name' 
-    bot.postMessageToUser('justin.carlson', ':battery: Puck battery low :battery:', params); 
-    bot.postMessageToUser('tristanmarshall', 'Puck battery low', params); 
+    // send notification to Tristan and I that the battery is low
+    slack.sendPM('justin.carlson', 'Puck battery low');
+    slack.sendPM('tristanmarshall', 'Puck battery low');
 
     res.status(200).send("success");
 })
